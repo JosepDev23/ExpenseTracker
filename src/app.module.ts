b35b38dd143c21c446feabdb525272bf80f196f3
Damian <damianev12@gmail.com>
2023-12-01 20:49:08 +0100

    create transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionService } from './modules/transaction/transaction.service';
 import { TransactionController } from './modules/transaction/transaction.controller';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [TransactionModule],
   controllers: [AppController, TransactionController],
   providers: [AppService, TransactionService],
 })
