84805982f8747c10ea98e29d284fd857d067b008
Carla Lixiang Fernández Muñoz <carlalixiangfermu@gmail.com>
2023-12-01 19:03:10 +0100

    create getAllTransactions and getTransactionById

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from '@nestjs/common'
+import { TransactionService } from './transaction.service'
+import Transaction from './transaction.schema'
 
 @Controller('transaction')
-export class TransactionController {}
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  async getAllTransactions(): Promise<Transaction[]> {
+    return this.transactionService.findAll()
+  }
+
+  async getTransactionById(id: string): Promise<Transaction> {
+    return this.transactionService.findById(id)
+  }
+}
