d78847f054739e6d5f5dddfdc83016a8d1626dbd
Carla Lixiang Fernández Muñoz <carlalixiangfermu@gmail.com>
2023-12-01 18:44:38 +0100

    create getAll and getById in user.controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common'
+import { UserService } from './user.service'
+import User from './user.schema'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  async getAll(): Promise<User[]> {
+    return this.userService.findAll()
+  }
+
+  async getById(id: string): Promise<User> {
+    return this.userService.findById(id)
+  }
+}
