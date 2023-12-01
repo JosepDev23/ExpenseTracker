e5a4e603676b639b0acd5e67832cab901405f423
Damian <damianev12@gmail.com>
2023-12-01 18:30:10 +0100

    generate transtaction service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionService } from './modules/transaction/transaction.service';
+import { TransactionController } from './modules/transaction/transaction.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TransactionController],
+  providers: [AppService, TransactionService],
 })
 export class AppModule {}
