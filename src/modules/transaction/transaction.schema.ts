b863bb0db9d5eee7f404563ceb9ea585adc1f26c
Damian <damianev12@gmail.com>
2023-12-03 20:26:14 +0100

    add schemas ApiProperty

--- src/modules/transaction/transaction.schema.ts
+++ src/modules/transaction/transaction.schema.ts
@@ -1,17 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { ApiProperty } from '@nestjs/swagger'
 
 @Schema()
 export default class Transaction {
   @Prop()
+  @ApiProperty()
   name: string
 
   @Prop()
+  @ApiProperty()
   ammount: number
 
   @Prop()
+  @ApiProperty()
   date: Date
 
   @Prop()
+  @ApiProperty()
   description: string
 }
 
--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,17 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { ApiProperty } from '@nestjs/swagger'
 
 @Schema()
 export default class User {
   @Prop()
+  @ApiProperty()
   username: string
 
   @Prop()
+  @ApiProperty()
   password: string
 
   @Prop()
+  @ApiProperty()
   phoneNumber: number
 
   @Prop()
+  @ApiProperty()
   currency: number
 }
 
