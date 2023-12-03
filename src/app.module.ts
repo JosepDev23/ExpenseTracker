15b900e558d564a9930dbeb4a258d3956e31e0a3
JosepDev23 <josepdev23@gmail.com>
2023-12-03 19:58:02 +0100

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { TransactionModule } from './modules/transaction/transaction.module'
+import { UserModule } from './modules/user/user.module'
 
 @Module({
-  imports: [TransactionModule, UserModule
+  imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.r1btshc.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
     ),
+    TransactionModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
