7f333e109d109441cbc3f2b0bacaf6ad56a4e145
Damian <damianev12@gmail.com>
2023-12-04 16:55:24 +0100

    jwt user module setup

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,14 @@ import { UserController } from './user.controller'
 import { UserService } from './user.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import User, { UserSchema } from './user.schema'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
   ],
   controllers: [UserController],
   providers: [UserService],
